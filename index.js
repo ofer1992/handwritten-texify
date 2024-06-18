72588f73482e2cadc3904488c5fd552cf79be685
ofer1992 <geon.arcan@gmail.com>
2024-06-18 18:52:24 +0300

    added raw latex and copy

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Reference the elements that we will need
 const status = document.getElementById('status');
 const latexContainer = document.getElementById('latex');
+const rawLatexContainer = document.getElementById('raw-latex');
+const copyButton = document.getElementById('copy-button');
+
 
 // Create a new worker
 const worker = new Worker('worker.js', {type: 'module'});
@@ -12,6 +15,7 @@ worker.onmessage = function (e) {
     }
     if (e.data.latex) {
         latexContainer.textContent = e.data.latex;
+        rawLatexContainer.textContent = e.data.latex;
         MathJax.typesetPromise([latexContainer]);
     }
 };
@@ -23,3 +27,13 @@ worker.postMessage('loadModel');
 export function detect(img) {
     worker.postMessage({ img: img });
 }
+
+// Copy to clipboard function
+copyButton.addEventListener('click', () => {
+    const text = rawLatexContainer.textContent;
+    navigator.clipboard.writeText(text).then(() => {
+        // alert('Copied to clipboard!');
+    }).catch(err => {
+        console.error('Could not copy text: ', err);
+    });
+});
